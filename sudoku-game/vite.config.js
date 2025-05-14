b333609009c2cedaed18c042f7587fd4d780f4a6
Heesoo Kang <audentia16@gmail.com>
2025-05-14 09:47:29 +0900

    vite.config.js

--- sudoku-game/vite.config.js
+++ sudoku-game/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/MiniGames/sudoku-game/',
+  build: {
+    outDir: resolve(__dirname, '../dist/sudoku-game')
+  },
   plugins: [react()],
 });
